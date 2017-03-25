be9c8588dda17e42514de07fb87e17120d1d5f3d
Michael Mokrysz <hi@46b.it>
2017-03-25 13:32:40 +0000

    Tidied ChatServer future a little.

--- src/server.rs
+++ src/server.rs
@@ -28,24 +28,20 @@ impl Future for ChatServer {
         println!("ChatServer wakes up with connected IDs {:?}",
                  self.clients.ids());
 
+        // Accept new clients.
         match self.incoming.poll() {
             Ok(Async::NotReady) => {}
             Ok(Async::Ready(Some((socket, addr)))) => {
+                // Assign incrementing IDs to clients.
                 let client_id = self.id_counter;
                 self.id_counter += 1;
                 println!("New client addr={:?} id={:?}", addr, client_id);
+                // Add new client to the Room.
                 let client = Client::new(client_id, socket.framed(StringCodec));
                 self.clients.insert(client);
             }
             Ok(Async::Ready(None)) => return Err("Incoming tcp client Stream died.".to_string()),
             Err(e) => return Err(format!("Incoming tcp client Stream error: {:?}", e)),
-        }
-
-        match self.clients.poll_complete() {
-            Ok(Async::NotReady) | Ok(Async::Ready(())) => {}
-            Err(e) => {
-                println!("Client Room Sink poll_complete error: {:?}", e);
-            }
         }
 
         loop {
@@ -57,10 +53,11 @@ impl Future for ChatServer {
                 }
                 Ok(Async::Ready(None)) => return Err("Client Room Stream poll died.".to_string()),
                 Err(e) => {
-                    println!("Client Room Stream error: {:?}", e);
+                    println!("Client Room Stream error:\n    {:?}", e);
                 }
             }
             if let Some((id, msg)) = rcvd {
+                // Broadcast this received message to all other clients.
                 for to_id in self.clients.ids() {
                     if id == to_id {
                         continue;
@@ -68,23 +65,29 @@ impl Future for ChatServer {
                     match self.clients.start_send((to_id, msg.clone())) {
                         Ok(AsyncSink::Ready) => {}
                         Ok(AsyncSink::NotReady((to_id, msg))) => {
+                            // No buffering is performed inside `Room` or `Client`.
                             println!("Dropped message. to_id={:?} msg={:?}", to_id, msg);
                         }
                         Err(e) => {
-                            println!("Client Room Sink start_send error: {:?}", e);
-                        }
-                    }
-                    match self.clients.poll_complete() {
-                        Ok(Async::NotReady) | Ok(Async::Ready(())) => {}
-                        Err(e) => {
-                            println!("Client Room Sink poll_complete error: {:?}", e);
+                            // Room's impl on Sink returns non-fatal errors. These indicate
+                            // a client disconnection, or an unknown client ID.
+                            println!("Client Room Sink start_send error:\n    {:?}", e);
                         }
                     }
                 }
             }
         }
 
+        // Make progress sending to all clients.
+        match self.clients.poll_complete() {
+            Ok(Async::NotReady) | Ok(Async::Ready(())) => {}
+            Err(e) => {
+                println!("Client Room Sink poll_complete error:\n    {:?}", e);
+            }
+        }
+
         println!("ChatServer quietens down");
+        // Always NotReady, unless an unrecoverable error happens.
         Ok(Async::NotReady)
     }
 }
